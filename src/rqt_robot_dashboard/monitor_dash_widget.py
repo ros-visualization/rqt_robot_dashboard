013805c1601efac61e4da5b82e3445283870db9e
Karsten Knese <Karsten.knese@googlemail.com>
2015-08-18 11:53:50 +0200

    put topic diagnostic_agg into a global namespace

--- src/rqt_robot_dashboard/monitor_dash_widget.py
+++ src/rqt_robot_dashboard/monitor_dash_widget.py
@@ -119,7 +119,7 @@ class MonitorDashWidget(IconToolButton):
                     self._monitor_shown = False
                 else:
                     self._monitor = RobotMonitorWidget(self.context,
-                                                       'diagnostics_agg')
+                                                       '/diagnostics_agg')
                     if self._plugin_settings:
                         self._monitor.restore_settings(self._plugin_settings,
                                                        self._instance_settings)
