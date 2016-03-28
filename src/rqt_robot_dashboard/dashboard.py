dac7029f8317c9dc0643d17cf7364b0fb7b83c3c
Eric Relson <erelson@fetchrobotics.com>
2016-03-28 15:00:27 -0700

    Add support for widget group labels
    To implement this, widgets are now added to QGroupBox elements.
    For whatever reason, this requires that widgets have their
    setIconSize set, or they appear very small.

--- src/rqt_robot_dashboard/dashboard.py
+++ src/rqt_robot_dashboard/dashboard.py
@@ -30,9 +30,8 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-
-from python_qt_binding.QtCore import QSize
-from python_qt_binding.QtGui import QToolBar
+from python_qt_binding.QtCore import QSize, Qt
+from python_qt_binding.QtGui import QToolBar, QGroupBox, QHBoxLayout
 from qt_gui.plugin import Plugin
 
 
@@ -58,18 +57,9 @@ class Dashboard(Plugin):
         self._main_widget.setWindowTitle(self.name)
         if context.serial_number() > 1:
             self._main_widget.setWindowTitle(self._main_widget.windowTitle() + (' (%d)' % context.serial_number()))
-        widgets = self.get_widgets()
 
-        self._widgets = []
-        for v in widgets:
-            for i in v:
-                try:
-                    self._main_widget.addWidget(i)
-                    self._widgets.append(i)
-                except:
-                    raise Exception("All widgets must be a subclass of QWidget!")
-
-            self._main_widget.addSeparator()
+        # Convert list of widgets into layout
+        self.add_widgets()
 
         # Display the dashboard
         context.add_toolbar(self._main_widget)
@@ -109,6 +99,49 @@ class Dashboard(Plugin):
         Most of the dashboard customization should be done here.
         If this function is not overriden the dashboard will display nothing.
 
-        :returns: List of lists containing dashboard widgets.
+        :returns: List of lists containing dashboard widgets, or list of lists
+                  containing a string followed by a list of dashboard widgets.
         """
         return []
+
+    def add_widgets(self):
+        """
+        Add groups of widgets to _main_widget. Supports group labels.
+
+        This method can be reimplemented in order to customize appearances.
+        """
+        widgets = self.get_widgets()
+        self._widgets = [] # stores widgets which may need to be shut down when done
+        for group in widgets:
+            # Check for group label
+            if isinstance(group[0], str):
+                grouplabel, v = group
+                box = QGroupBox(grouplabel)
+                box.setContentsMargins(0, 18, 0, 0) # LTRB
+                # Apply the center-label directive only for single-icon groups
+                if len(group[1]) == 1:
+                    box.setAlignment(Qt.AlignHCenter)
+            else:
+                box = QGroupBox()
+                box.setContentsMargins(0, 0, 0, 0) # LTRB
+                v = group
+            # Add widgets to QGroupBox
+            layout = QHBoxLayout()
+            layout.setSpacing(0)
+            layout.setContentsMargins(0, 0, 0, 0) # LTRB
+            for i in v:
+                try:
+                    try:
+                        i.setIconSize(self.max_icon_size) # without this, icons are tiny
+                    except AttributeError as e:
+                        # triggers with battery which uses a QLabel instead of a QToolButton-based widget
+                        pass
+                    layout.addWidget(i)
+                    self._widgets.append(i)
+                except:
+                    raise Exception("All widgets must be a subclass of QWidget!")
+
+            layout.activate()
+            box.setLayout(layout)
+            self._main_widget.addWidget(box)
+            self._main_widget.addSeparator()
