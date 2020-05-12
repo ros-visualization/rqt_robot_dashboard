e2ad17752f7aa32a6e026f95183ec1565b30276a
Shane Loretz <sloretz@osrfoundation.org>
2020-05-12 13:22:29 -0700

    Use setuptools always
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-try:
-    from distutils.core import setup
-except ImportError:
-    from setuptools import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
